5299424c49e751105d973bed429c1caf20405fa5
Sidali <sidali.dev@gmail.com>
2025-04-14 14:01:20 +0100

    third commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,9 +143,10 @@ const pages = document.querySelectorAll("[data-page]");
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
-
+    const targetPage = this.innerHTML.trim().toLowerCase();
+    
     for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+      if (targetPage === pages[i].dataset.page) {
         pages[i].classList.add("active");
         navigationLinks[i].classList.add("active");
         window.scrollTo(0, 0);
@@ -154,6 +155,5 @@ for (let i = 0; i < navigationLinks.length; i++) {
         navigationLinks[i].classList.remove("active");
       }
     }
-
   });
 }
